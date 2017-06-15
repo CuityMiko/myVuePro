afc7beecb988cc6eebd0d2542bd13a6acfc8fe8e
CuityMiko <15201776956@163.com>
2017-06-15 09:31:41 +0800

    动态路由匹配

--- src/containers/MovieContainer.vue
+++ src/containers/MovieContainer.vue
@@ -5,6 +5,9 @@
 <template>
     <div>
         <h3>{{title}}</h3>
+        <p>
+            <router-link to="/moviedetail/1000?name=新木乃伊">电影详情</router-link>
+        </p>
     </div>
 </template>
 
---
+++ src/containers/MovieDetailContainer.vue
@@ -0,0 +1,34 @@
+<style>
+
+</style>
+
+<template>
+    <div>
+        <h3>{{title}}</h3>
+        <h5>电影ID：{{id}}，电影名称：{{name}}</h5>
+        <p>
+            
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            id:this.$route.params.id,
+            name:this.$route.query.name,
+            title:'电影详情'
+        }
+    },
+    watch:{
+        '$route':(to,from)=>{
+
+        }
+    },
+    mounted(){
+        console.log(this.$route.params.id)
+        console.log(this.$route.query.name)
+    }
+}
+</script>
