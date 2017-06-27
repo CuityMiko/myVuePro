25d78603661e47c4fb3f0f07e634cbea47a88bcb
CuityMiko <15201776956@163.com>
2017-06-27 20:50:31 +0800

    transition-group使用

--- src/components/examples/AnimateCssComponent.vue
+++ src/components/examples/AnimateCssComponent.vue
@@ -4,8 +4,15 @@
 <template>
     <div>
         <h3>Animate.css动画</h3>
+        <!--transition-group 是针对多个元素标签进行动画的，transition只能针对一个标签-->
+        <input type="text" v-model.trim="search">
+        <transition-group enter-active-class="animated bounceInLeft" leave-active-class="animated bounceOutRight">
+            <div v-for="(stu,index) in students" :key="index">
+                <h1>{{stu}}</h1>
+            </div>
+        </transition-group>
         <transition enter-active-class="animated bounceInLeft" leave-active-class="animated bounceOutRight">
-            <div v-show="todh" transition='myh1'>
+            <div v-show="todh">
                 <h1>Animate.css</h1>
             </div>
         </transition>
@@ -18,7 +25,21 @@ import '../../bower_components/animate.css/animate.css'
 export default {
     data(){
         return{
-            todh:true
+            todh:true,
+            student:['张三','赵四','王三','赵六'],
+            search:''
+        }
+    },
+    computed:{
+        students(){
+            let _list=[];
+            let that=this;
+            that.student.forEach((item)=>{
+                if(that.search.length>0 && item.indexOf(that.search)>-1){
+                    _list.push(item);
+                }
+            });
+            return _list;
         }
     },
     methods:{
