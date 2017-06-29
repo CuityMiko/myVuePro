9472232d43d82e84790345ebaa510e4595358ba8
CuityMiko <15201776956@163.com>
2017-06-29 10:40:46 +0800

    修改server

--- server/dependencies/cores.js
+++ server/dependencies/cores.js
@@ -3,7 +3,5 @@ module.exports = function (core) {
     core.tools = require('../core/tools');
     core.http = require('../core/httpHelper');
     core.request = require('../core/requestHelper');
-    core.businessConvert = require('../core/businessConvert');
-    core.businessMethod = require('../core/businessMethod');
     core.mongodb = require('../core/mongodb');
 }
